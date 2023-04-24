00cd97cb73f17e11f8e29a7a30953d394034f47b
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-24 14:42:00 -0300

    feat: transferindo dados para o componente filho

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  //string com type inference
+  name: string = 'Alexandre';
+
+  //number com type inference
+  age: number = 30;
+
+  //string sem type inference
+  job = 'Programador';
+
+  //array
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+
+  //object
+  car = {
+    name: "Polo",
+    year: 2019,
+  };
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.css']
+})
+export class ParentDataComponent implements OnInit {
+  @Input() name: string = '';
+  @Input() userData!: {email: string, role: string};
+  // cuidado que ao usar ! você está garantindo que o dado será inicializado
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+}
